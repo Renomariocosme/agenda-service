6290f45b462260dece42034697baa67da8559200
Renomario Cosme Silva <rcosmesi@emeal.nttdata.com>
2022-10-28 14:56:03 -0300

    Configurando projeto

---
+++ src/test/java/pdev/com/agendaapi/AgendaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pdev.com.agendaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
