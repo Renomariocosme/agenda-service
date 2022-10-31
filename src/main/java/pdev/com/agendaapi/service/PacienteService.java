82f2547077ebe1d38501d9579b2de0bbe3138140
Renomario Cosme Silva <rcosmesi@emeal.nttdata.com>
2022-10-31 11:43:38 -0300

    criando classe de serviço

--- src/main/java/pdev/com/agendaapi/service/PacienteService.java
+++ src/main/java/pdev/com/agendaapi/service/PacienteService.java
@@ -1,11 +1,13 @@
 package pdev.com.agendaapi.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pdev.com.agendaapi.domain.entities.Paciente;
 import pdev.com.agendaapi.repository.PacienteRepository;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,5 +16,23 @@ public class PacienteService {
 
     private final PacienteRepository repository;
 
-    
+    public Paciente salvar(Paciente obj){
+
+        //valida CPF se já existe ou email
+        if (obj.getEmail().){
+
+        }
+
+
+        return repository.save(obj);
+    }
+    public List<Paciente> buscarTodos(){
+        return repository.findAll();
+    }
+    public Optional<Paciente> buscarPorId(Long id){
+        return repository.findById(id);
+    }
+    public void deletar(Long id){
+        repository.deleteById(id);
+    }
 }
