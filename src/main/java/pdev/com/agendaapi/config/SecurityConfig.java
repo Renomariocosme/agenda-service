b05afb05995b57c5f6035bdfc8a0628966b8f6a8
Renomario cosme <renomario.cosme@gmail.com>
2022-11-17 18:07:30 -0300

    Segurança JWT - Classes de config

--- src/main/java/pdev/com/agendaapi/config/SecurityConfig.java
+++ src/main/java/pdev/com/agendaapi/config/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         // autenticacao
         http.addFilter(new CustomAuthenticationFilterConfig(authenticationManager()));
+
         // autorização
         // authenticationManager
     }
